d1830e4e8d9b9b1afe7b3dad413ec5810fa4fb54
victhor <ntthang9526@gmail.com>
2026-08-12 15:18:39 +0700

    Khoi tao class SinhVien

---
+++ Buoi1/QuanLySinhVien/SinhVien.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySinhVien
+{
+    internal class SinhVien
+    {
+        public String MaSinhVien { get; set; }
+        public String HoTen { get; set; }
+        public String NgaySinh { get; set; }
+        public String GioiTinh { get; set; }
+        public String Email { get; set; }
+        public String SoDienThoai { get; set; }
+        public String NganhHoc { get; set; }
+
+        private float _diemTrungBinh;
+        public float DiemTrungBinh
+        {
+            get { return _diemTrungBinh; }
+            set
+            {
+                if (value < 0 || value > 10)
+                {
+                    Console.WriteLine("Diem khong hop le!");
+                    return;
+                }
+                _diemTrungBinh = value;
+            }
+        }
+
+        public String TrangThaiHocTap { get; set; }
+
+    }
+}
